82a80b3002ad2329fd259c797da425582cabf3f8
sheida <sheidasepehri01@gmail.com>
2022-06-22 17:25:04 +0430

    Updates

---
+++ precache-manifest.abc39521e5eaa7823cb09312be84b524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c001e32fd9a3ce5017a2",
+    "url": "/clock/static/css/main.16672e53.chunk.css"
+  },
+  {
+    "revision": "c001e32fd9a3ce5017a2",
+    "url": "/clock/static/js/main.c001e32f.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/clock/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "bd8387231a6637a9e874",
+    "url": "/clock/static/js/2.bd838723.chunk.js"
+  },
+  {
+    "revision": "6429cee60a1d7e481321",
+    "url": "/clock/static/js/runtime~main.6429cee6.js"
+  },
+  {
+    "revision": "ad51b2dacdf0f418f22dbee2a8f4a93d",
+    "url": "/clock/index.html"
+  }
+];
